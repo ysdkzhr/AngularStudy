115081dc91a6ac5e89e080b76061677a9f37a2e0
K.Yoshida <yoshida.kazuhiro@chiyodacorp.com>
2019-07-08 12:36:50 +0900

    add product alert component

--- AngularExample/src/app/app.module.ts
+++ AngularExample/src/app/app.module.ts
@@ -6,19 +6,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertComponent } from './product-alert/product-alert.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent
-  ],
-  bootstrap: [AppComponent]
+   imports: [
+      BrowserModule,
+      ReactiveFormsModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   declarations: [
+      AppComponent,
+      TopBarComponent,
+      ProductListComponent,
+      ProductAlertComponent,
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
